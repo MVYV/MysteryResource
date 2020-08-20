7d7b2925df9a973878af3f06752f60ad4cfb8031
MichaelValkiv <mishavalkiv@gmail.com>
2020-08-20 22:03:02 +0300

    Home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HomeOverallComponent } from './home/home-overall/home-overall.component';
+import { CarouselModule } from 'primeng/carousel';
 
 
 @NgModule({
     declarations: [HomeComponent, HomeOverallComponent],
     imports: [
         CommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        CarouselModule
     ]
 })
 export class HomeModule {
--- src/app/modules/home/home/home-overall/home-overall.component.ts
+++ src/app/modules/home/home/home-overall/home-overall.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home-overall',
-  templateUrl: './home-overall.component.html',
-  styleUrls: ['./home-overall.component.scss']
+    selector: 'app-home-overall',
+    templateUrl: './home-overall.component.html',
+    styleUrls: ['./home-overall.component.scss']
 })
 export class HomeOverallComponent implements OnInit {
+    public homeCarousel = [
+        {
+            name: 'Civilization VI'
+        },
+        {
+            name: 'Civilization III'
+        },
+        {
+            name: 'FIFA 20'
+        },
+        {
+            name: 'Counter Strike Global Offencive'
+        },
+        {
+            name: 'FIFA 19'
+        },
+        {
+            name: 'Civilization II'
+        },
+        {
+            name: 'Civilization V'
+        }
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
