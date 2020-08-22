9a5f1f3d2c1e4274509bb47c6673a5216f3c56df
Misha <mishavalkiv@gmail.com>
2020-08-22 19:28:37 +0300

    Registration form start

--- src/app/modules/authentication/authentication/registration/registration.component.ts
+++ src/app/modules/authentication/authentication/registration/registration.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+    selector: 'app-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+    public registrationForm = this.formBuilder.group({
+        firstNameControl: ['', [
+            Validators.required,
+            Validators.minLength
+        ]],
+        lastNameControl: ['', [
+            Validators.required,
+            Validators.minLength
+        ]],
+        userNameControl: ['', [
+            Validators.required,
+            Validators.minLength
+        ]],
+        emailControl: ['', [
+            Validators.required,
+            Validators.email
+        ]],
+        cityControl: ['', [
+            Validators.required,
+            Validators.minLength
+        ]],
+        countryControl: ['', [
+            Validators.required,
+            Validators.minLength
+        ]],
+        dateOfBirthControl: ['', [
+            Validators.required
+        ]],
+        genderControl: ['', [
+            Validators.required
+        ]],
+        passwordControl: ['', [
+            Validators.required,
+            Validators.minLength
+        ]],
+        rolesControl: ['', [
+            Validators.required
+        ]]
+    });
 
-  ngOnInit(): void {
-  }
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+    }
+
+    public submitRegistration() {
+    }
 
 }
